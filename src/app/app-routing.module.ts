594b5cb26744f9af205fb979292462c83a470518
Alice López Gómez <alicelopezgomez@macbook-air-de-antonio.home>
2020-11-18 21:30:05 +0100

    generación de componentes y routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
+import { ConceptsComponent } from './components/concepts/concepts.component';
+import { CountryComponent } from './components/country/country.component';
+import { CreatorComponent } from './components/creator/creator.component';
+import { ErrorComponent } from './components/error/error.component';
+import { FormComponent } from './components/form/form.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MapComponent } from './components/map/map.component';
+import { PostComponent } from './components/post/post.component';
+import { ResultComponent } from './components/result/result.component';
+import { TestComponent } from './components/test/test.component';
+import { TestsComponent } from './components/tests/tests.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HomeComponent },
-
-
+  { path: '', component: HomeComponent },
+  { path: '/mapa', component: MapComponent },
+  { path: '/mapa/pais/:id', component: CountryComponent },
+  { path: '/conceptos', component: ConceptsComponent },
+  { path: '/blog/general', component: BlogComponent },
+  { path: '/blog/general/:id', component: PostComponent },
+  { path: '/blog/hablando-de-politica', component: BlogComponent },
+  { path: '/blog/hablando-de-politica/:id', component: PostComponent },
+  { path: '/blog/new', component: FormComponent },
+  { path: '/test', component: TestsComponent },
+  { path: '/test/:id', component: TestComponent },
+  { path: '/test/:id/resultado', component: ResultComponent },
+  { path: '/login', component: LoginComponent },
+  { path: '/usuario/:id', component: UserComponent },
+  { path: '/nosotros', component: CreatorComponent },
+  { path: '**', component: ErrorComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,41 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
+import { CreatorComponent } from './components/creator/creator.component';
+import { UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormComponent } from './components/form/form.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { PostComponent } from './components/post/post.component';
+import { MapComponent } from './components/map/map.component';
+import { CountryComponent } from './components/country/country.component';
+import { ConceptsComponent } from './components/concepts/concepts.component';
+import { TestsComponent } from './components/tests/tests.component';
+import { TestComponent } from './components/test/test.component';
+import { ResultComponent } from './components/result/result.component';
+import { RankingComponent } from './components/ranking/ranking.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ErrorComponent,
+    CreatorComponent,
+    UserComponent,
+    LoginComponent,
+    FormComponent,
+    BlogComponent,
+    PostComponent,
+    MapComponent,
+    CountryComponent,
+    ConceptsComponent,
+    TestsComponent,
+    TestComponent,
+    ResultComponent,
+    RankingComponent
   ],
   imports: [
     BrowserModule,
