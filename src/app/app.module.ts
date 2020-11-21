71ea370c5cd76fda1bd308f6e8ba364e50db005d
Alice López Gómez <alicelopezgomez@macbook-air-de-antonio.home>
2020-11-21 17:48:00 +0100

    traductor y logica de idiomas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+/* HEADER & FOOTER */
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+/* HOME */
 import { HomeComponent } from './components/home/home.component';
+
+/* ERROR */
 import { ErrorComponent } from './components/error/error.component';
+
+/* NOSOTROS */
 import { CreatorComponent } from './components/creator/creator.component';
+
+/* USUARIO */
 import { UserComponent } from './components/user/user.component';
 import { LoginComponent } from './components/login/login.component';
+
+/* BLOG */
 import { FormComponent } from './components/form/form.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { PostComponent } from './components/post/post.component';
+
+/* MAPA */
 import { MapComponent } from './components/map/map.component';
 import { CountryComponent } from './components/country/country.component';
+
+/* CONCEPTO */
 import { ConceptsComponent } from './components/concepts/concepts.component';
+
+/* TEST */
 import { TestsComponent } from './components/tests/tests.component';
 import { TestComponent } from './components/test/test.component';
 import { ResultComponent } from './components/result/result.component';
 import { RankingComponent } from './components/ranking/ranking.component';
+
+/* NGBOOTSTRAP */
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+/* TRADUCTOR   */
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
@@ -46,7 +71,17 @@ import { RankingComponent } from './components/ranking/ranking.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => {
+          return new TranslateHttpLoader(http);
+        },
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
