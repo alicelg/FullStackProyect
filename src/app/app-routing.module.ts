c50a61e34350ec1c952440b7b708c87da9022ee6
Alice López Gómez <alicelopezgomez@macbook-air-de-antonio.home>
2020-11-24 21:33:29 +0100

    corregido enlace tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
   { path: 'blog/hablando-de-politica', component: BlogComponent },
   { path: 'blog/hablando-de-politica/:id', component: PostComponent },
   { path: 'blog/new', component: FormComponent },
-  { path: 'test', component: TestsComponent },
+  { path: 'tests', component: TestsComponent },
   { path: 'test/:id', component: TestComponent },
   { path: 'test/:id/resultado', component: ResultComponent },
   { path: 'login', component: LoginComponent },
