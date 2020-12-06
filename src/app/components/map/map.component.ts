2e47ecfc6411e125e3ec90e5bf82cb368f796d3d
Alice López Gómez <alicelop@ucm.es>
2020-12-06 18:35:55 +0100

    agregado mapa estilos y logica

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
+import { count } from 'console';
+import * as jsVectorMap from 'jsvectormap/dist/js/jsvectormap.js';
+import 'jsvectormap/dist/maps/world.js';
+import { CountryMap } from '../../models/countryMap.model';
+// import 'jsvectormap/dist/maps/world'
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -7,15 +12,98 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  countrySelected;
+  selectedCountries: CountryMap[] = [];
+  map;
 
-  constructor() { }
+  constructor(
+    private translateService: TranslateService
+  ) { }
 
   ngOnInit(): void {
+
+    this.map = new jsVectorMap({
+      // tipología y configuración del mapa
+      map: 'world',
+      selector: '#map',
+
+      regionsSelectable: true,
+      markersSelectable: true,
+
+      // eventos del mapa
+      onRegionSelected: (index, isSelected, selectedRegions) => {
+        console.log(index, isSelected, selectedRegions);
+        // Obtenemos los datos completos del pais seleccionado
+        const countryMap = this.getCountryData(index);
+
+        // comprobamos si se esta seleccionando o des-seleccionando
+        if (isSelected) {
+          // si se esta seleccionando lo añadimos al array
+          this.selectedCountries.push(countryMap);
+
+        } else {
+          // si se esta des-seleccionando lo añadimos al array lo eliminamos
+          const countryIndex = this.selectedCountries.map(country => country.index).indexOf(index);
+          this.selectedCountries.splice(countryIndex, 1);
+        }
+      },
+
+      onRegionTooltipShow: (tooltip, code) => {
+        tooltip.css({ backgroundColor: '#000000' });
+
+        tooltip.selector.innerHTML =
+          `<div style='color:white'>
+            ${this.translateService.instant('COUNTRIES.' + code)}
+          </div>`;
+      },
+
+      // estilos del mapa
+      regionStyle: {
+        initial: {
+          fill: '#4d0707'
+        },
+        hover: {
+          fillOpacity: 1,
+          fill: '#901b22'
+        },
+        selected: {
+          fill: '#ff5566'
+        }
+      },
+
+      regionLabelStyle: {
+        initial: {
+          fontFamily: 'Verdana',
+          fontSize: '12',
+          fontWeight: 'bold',
+          cursor: 'default',
+          fill: '#ff5566'
+        },
+        hover: {
+          cursor: 'pointer'
+        }
+      }
+    });
+
   }
 
-  selectCountry(): void {
-    this.countrySelected = 'España';
+  clearSelectedCountries(): void {
+    this.selectedCountries = [];
+    this.map.clearSelectedRegions();
+
+  }
+
+  getCountryData(index): CountryMap {
+    const countryMap = new CountryMap();
+
+    countryMap.index = index;
+    countryMap.name = index;
+    countryMap.flag = '';
+    countryMap.president = '';
+    countryMap.language = '';
+    countryMap.money = '';
+
+    return countryMap;
+
   }
 
 }
---
+++ src/app/models/countryMap.model.ts
@@ -0,0 +1,11 @@
+import { Tracing } from 'trace_events';
+
+export class CountryMap {
+  index: string;
+  name: string;
+  flag: string;
+  president: string;
+  goverment: string;
+  language: string;
+  money: string;
+}
