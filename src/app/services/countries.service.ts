b0ab8542122944e01f186892b3936fe0c7a079a4
Alice López Gómez <alicelop@ucm.es>
2020-12-12 20:55:31 +0100

    añadida logica de selección de pais

--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { CountryMap } from '../models/countryMap.model';
+import { BehaviorSubject } from 'rxjs';
+import { Country } from '../models/country.model';
 
 
 @Injectable({
@@ -15,14 +15,26 @@ export class CountriesService {
     private httpClient: HttpClient
   ) {
     this.baseUrl = 'http://localhost:3000/countries';
-   }
+  }
+
+
+  selectedCountry = new BehaviorSubject(this.isSelected);
+
+  set isSelected(value) {
+    this.selectedCountry.next(value);
+    value === null ? localStorage.removeItem('selectedCountry') : localStorage.setItem('selectedCountry', JSON.stringify(value));
+  }
+
+  get isSelected(): Country {
+    return JSON.parse(localStorage.getItem('selectedCountry'));
+  }
 
   getCountryFlagByCode(code): Promise<any> {
     return this.httpClient.get<any>('https://restcountries.eu/rest/v2/alpha/' + code).toPromise();
   }
 
-  getCountriesDataByCode(code): Promise<any> {
-    return this.httpClient.get<CountryMap[]>(this.baseUrl, { params: { code } }).toPromise();
+  getCountriesDataByCode(code): Promise<Country[]> {
+    return this.httpClient.get<Country[]>(this.baseUrl, { params: { code } }).toPromise();
   }
 }
 
