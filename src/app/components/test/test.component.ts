33ff07dd99746e7df1588321d1a577c8af5bc959
Alice López Gómez <alicelop@ucm.es>
2020-12-13 04:25:28 +0100

    test banderas funcionando salvo guardado

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { Answer, Question } from 'src/app/models/question.model';
 import { User } from 'src/app/models/user.model';
 import { TestsService } from 'src/app/services/tests.service';
 import { UsersService } from 'src/app/services/users.service';
@@ -10,30 +12,71 @@ import { UsersService } from 'src/app/services/users.service';
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
-  step: string;
+  action: string;
   currentUser: User;
   testId: string;
+
+  allQuestions: Question[];
+  actualQuestion: Question;
+
+  questionNumber: number;
+
+  svg: SafeHtml;
+
+  answersArray: Answer[];
+
 
   constructor(
     private userService: UsersService,
     private testService: TestsService,
     private activatedRoute: ActivatedRoute,
+    private sanitizer: DomSanitizer
   ) {
-    this.step = 'instructions';
+
+    this.action = 'instructions';
+
     this.userService.currentUser.subscribe(res => {
       this.currentUser = res;
     });
 
+    this.answersArray = [];
+
     this.testId = this.activatedRoute.snapshot.paramMap.get('testId');
+    this.questionNumber = 0;
   }
 
   ngOnInit(): void {
-
     this.testService.getQuestionsByTest(this.testId).then(res => {
       console.log(res);
-
+      this.allQuestions = res;
+      this.actualQuestion = this.allQuestions[this.questionNumber];
+      this.svg = this.sanitizer.bypassSecurityTrustHtml(this.actualQuestion.image);
     });
 
   }
 
+  questionAnswered(questionId, answerId): void {
+    const answer = new Answer();
+    answer.questionId = questionId;
+    answer.answerId = answerId;
+
+    this.answersArray.push(answer);
+
+    if (this.questionNumber < this.allQuestions.length - 1) {
+      this.nextQuestion();
+    } else {
+      console.log(this.answersArray);
+      this.testService.setAnswers(this.testId, this.answersArray).then(res => {
+        console.log(res);
+
+      })
+    }
+
+  }
+
+  nextQuestion(): void {
+    this.questionNumber++;
+    this.actualQuestion = this.allQuestions[this.questionNumber];
+    this.svg = this.sanitizer.bypassSecurityTrustHtml(this.actualQuestion.image);
+  }
 }
