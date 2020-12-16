8cbcfba82eaafd4a60924731b9aaa1484cf8d2b8
alicelg <alicelop@ucm.es>
2020-12-16 12:21:29 +0100

    modelo del post

---
+++ src/app/models/post.model.ts
@@ -0,0 +1,13 @@
+export class Post {
+
+    title: string;
+    main_image: string;
+    keywords: string;
+    summary: string;
+    category: string;
+    test: string;
+    id: number;
+    date: Date;
+    type: number;
+
+}
