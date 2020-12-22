794a936ada3b3626c1aa4f0cccbe585d167bac02
Alice López Gómez <alicelop@ucm.es>
2020-12-23 00:10:52 +0100

    busqueda de paises corregida

--- src/app/components/country/country.component.ts
+++ src/app/components/country/country.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Country } from 'src/app/models/country.model';
 import { CountriesService } from 'src/app/services/countries.service';
 
@@ -10,14 +11,31 @@ import { CountriesService } from 'src/app/services/countries.service';
 export class CountryComponent implements OnInit {
 
   country: Country;
+  countryCode: string;
+  search: string;
+
   constructor(
     private countriesService: CountriesService,
+    private avtivatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.countriesService.selectedCountry.subscribe(res => {
-      this.country = res;
-    });
-  }
+    this.search = this.avtivatedRoute.snapshot.queryParamMap.get('searchCountry');
 
+    if (this.search) {
+      this.countryCode = this.avtivatedRoute.snapshot.paramMap.get('code');
+      this.countriesService.getCountriesDataByCode([this.countryCode]).then(data => {
+        this.country = data[0];
+        this.country.languages = this.country.languages.split(',').join(', ');
+        this.countriesService.getCountryFlagByCode(this.countryCode).then(res => {
+          this.country.flag = res.flag;
+          this.country.currencies = res.currencies.map(currency => currency.name).join(', ');
+        });
+      });
+    } else {
+      this.countriesService.selectedCountry.subscribe(res => {
+        this.country = res;
+      });
+    }
+  }
 }
